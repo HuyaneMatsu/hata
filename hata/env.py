3c7c54cf0492a5bb602ac22159a180c2533c5ab9
HuyaneMatsu <re.ism.tm@gmail.com>
2021-11-18 02:57:55 +0100

    resort code

--- hata/env.py
+++ hata/env.py
@@ -1,9 +1,6 @@
 ﻿"""
 Before loading hata, it checks for related environmental variables, which are:
 
-HATA_BACKEND_ONLY : `bool` = `False`
-    Whether `hata.discord` should not be imported as well.
-
 HATA_ALLOW_DEAD_EVENTS : `bool` = `False`
     Whether events of non cached entities should be handled. Affects the following events right now:
     
@@ -13,6 +10,18 @@ HATA_ALLOW_DEAD_EVENTS : `bool` = `False`
     - `Client.events.reaction_clear`
     - `Client.events.reaction_delete`
     - `Client.events.reaction_delete_emoji`
+
+HATA_API_ENDPOINT : `None`, `str` = `None`
+    The api endpoint to use instead of the Discord's default.
+
+HATA_API_VERSION : `int` = `9`
+    The Discord api version used by hata. The accepted values are `6`, `7`, `8`, `9` and `10`.
+    
+    If given as any other value, a warning message will show up. Tho, if given `6` a deprecation warning will be still
+    present.
+
+HATA_BACKEND_ONLY : `bool` = `False`
+    Whether `hata.discord` should not be imported as well.
 
 HATA_CACHE_PRESENCE : `bool` = `True`
     Whether hata should enable presence related attributes and dispatch users presence related events. By disabling it,
@@ -24,26 +33,11 @@ HATA_CACHE_PRESENCE : `bool` = `True`
 HATA_CACHE_USERS : `bool` = `True`
     Whether hata should cache users. Disabling it can cause many hata features to disappear.
 
-HATA_MESSAGE_CACHE_SIZE : `int` = `10`
-    The default message cache size per channel.
-
-HATA_API_ENDPOINT : `None`, `str` = `None`
-    The api endpoint to use instead of the Discord's default.
-
 HATA_CDN_ENDPOINT : `None`, `str` = `None`
     The cdn (content delivery network) endpoint to use instead of the Discord's default.
 
 HATA_DISCORD_ENDPOINT : `None`, `str` = `None`
     The endpoint of Discord, to use instead of it's own.
-
-HATA_STATUS_ENDPOINT : `None`, `str` = `None`
-    Discord status endpoint.
-
-HATA_API_VERSION : `int` = `9`
-    The Discord api version used by hata. The accepted values are `6`, `7`, `8`, `9` and `10`.
-    
-    If given as any other value, a warning message will show up. Tho, if given `6` a deprecation warning will be still
-    present.
 
 HATA_DOCS_ENABLED : `bool` = `True`
     Whether hata should be loaded with docstrings.
@@ -54,9 +48,15 @@ HATA_DOCS_ENABLED : `bool` = `True`
 
 HATA_LIBRARY_URL : `str` = `'https://github.com/HuyaneMatsu/hata'`
     Library url used at user agents.
-    
+
+HATA_MESSAGE_CACHE_SIZE : `int` = `10`
+    The default message cache size per channel.
+
 HATA_RICH_DISCORD_EXCEPTION : `bool` = `False`
     Whether ``DiscordException``-s should show the request data as well.
+
+HATA_STATUS_ENDPOINT : `None`, `str` = `None`
+    Discord status endpoint.
 """
 import warnings
 from os import getenv as get_environmental_variable
@@ -68,6 +68,9 @@ except ModuleNotFoundError:
 else:
     load_dotenv()
 
+__all__ = ('ALLOW_DEAD_EVENTS', 'API_VERSION', 'BACKEND_ONLY', 'CACHE_PRESENCE', 'CACHE_USER', 'CUSTOM_API_ENDPOINT',
+    'CUSTOM_CDN_ENDPOINT', 'CUSTOM_DISCORD_ENDPOINT', 'CUSTOM_STATUS_ENDPOINT', 'DOCS_ENABLED', 'LIBRARY_URL',
+    'MESSAGE_CACHE_SIZE', 'RICH_DISCORD_EXCEPTION')
 
 def get_bool_env(name, default, *, warn_if_empty=True):
     """

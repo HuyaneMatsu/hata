9de43a64ce0c75b85dd7a8dc0ce9015e2f11ed6b
HuyaneMatsu <re.ism.tm@gmail.com>
2022-06-02 07:35:33 +0200

    fix bug

--- hata/ext/plugin_loader/utils/register_plugin_.py
+++ hata/ext/plugin_loader/utils/register_plugin_.py
@@ -5,6 +5,6 @@ from scarletio import copy_docs
 from ..plugin_loader import PLUGIN_LOADER
 
 
-@copy_docs(PLUGIN_LOADER.register_plugin)
+@copy_docs(PLUGIN_LOADER.register)
 def register_plugin(name, *positional_parameters, **keyword_parameters):
     return PLUGIN_LOADER.register(name, *positional_parameters, **keyword_parameters)

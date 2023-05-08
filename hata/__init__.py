52536cd543c507008641b4f8fc4549ecb7b850fc
HuyaneMatsu <re.ism.tm@gmail.com>
2023-05-08 14:51:09 +0200

    increment version

--- hata/__init__.py
+++ hata/__init__.py
@@ -107,7 +107,7 @@ interrupt.
 
 We got some `tutorials:https://www.astil.dev/project/hata/guides/` as well, please check them too!
 """
-__version__ = '1.3.28'
+__version__ = '1.3.29'
 
 from .discord import *
 from .ext import *
